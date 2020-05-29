84cddd178af5fc742bc2f20b8a3d59671e7e9997
Saoirse Bryar <smbryar@gmail.com>
2020-05-29 09:30:14 +0100

    Updates

---
+++ precache-manifest.99fbd2d19dd1722b83d57c9863318259.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5cdd8fcddbd456b99b812f116dbabc1",
+    "url": "/AYTO-Season8-Simulator/index.html"
+  },
+  {
+    "revision": "e838af3f133c00303386",
+    "url": "/AYTO-Season8-Simulator/static/css/main.712d03e9.chunk.css"
+  },
+  {
+    "revision": "61213ec43053ec6161c7",
+    "url": "/AYTO-Season8-Simulator/static/js/2.3cd54805.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/AYTO-Season8-Simulator/static/js/2.3cd54805.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e838af3f133c00303386",
+    "url": "/AYTO-Season8-Simulator/static/js/main.2814b28d.chunk.js"
+  },
+  {
+    "revision": "3d9b08765f26656290ce",
+    "url": "/AYTO-Season8-Simulator/static/js/runtime-main.650e8838.js"
+  }
+]);
