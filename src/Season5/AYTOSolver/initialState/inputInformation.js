2c85b159ffb8ddcca9eecb9b746d4ba7d4415096
Saoirse Bryar <smbryar@gmail.com>
2020-06-18 21:55:20 +0100

    Add Season 5 Week 3 data

--- src/Season5/AYTOSolver/initialState/inputInformation.js
+++ src/Season5/AYTOSolver/initialState/inputInformation.js
@@ -5,7 +5,8 @@ const initialContestants = {
 
 const initialTruthBooths = [
     { week: 1, woman: "Gianna", man: "Hayden", correct: false },
-    { week: 2, woman: "Alicia", man: "Andre", correct: false }]
+    { week: 2, woman: "Alicia", man: "Andre", correct: false },
+    { week: 3, woman: "Carolina", man: "Ozzy", correct: false }]
 
 
 const initialMatchingCeremonies = [
@@ -22,6 +23,13 @@ const initialMatchingCeremonies = [
         { woman: "Gianna", man: "Michael" }, { woman: "Hannah", man: "Andre" }, { woman: "Kam", man: "Jaylan" }, 
         { woman: "Kari", man: "Osvaldo" }, { woman: "Kathryn", man: "Ozzy" }, { woman: "Shannon", man: "Edward" }, 
         { woman: "Taylor", man: "Hayden" }, { woman: "Tyranny", man: "Tyler" }]
+    },
+    {
+        week: 3, lights: 4,
+        pairs: [{ woman: "Alicia", man: "Mike" }, { woman: "Carolina", man: "Hayden" }, { woman: "Cas", man: "Jaylan" }, 
+        { woman: "Gianna", man: "Ozzy" }, { woman: "Hannah", man: "Derrick" }, { woman: "Kam", man: "Edward" }, 
+        { woman: "Kari", man: "Andre" }, { woman: "Kathryn", man: "Joey" }, { woman: "Shannon", man: "Tyler" }, 
+        { woman: "Taylor", man: "Michael" }, { woman: "Tyranny", man: "Osvaldo" }]
     }
 ];
 
